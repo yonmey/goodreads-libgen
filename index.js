0912631b20dd89ad8ad3ba099f06cc5c088afc0f
yonmey <yonmey@gmail.com>
2018-07-01 17:33:23 +0200

    Init

---
+++ index.js
@@ -0,0 +1,23 @@
+// SELECTORS
+const bookTitleSelector = 'h1#bookTitle';
+const btnContainerSelector = 'div#imagecol > div.wtrButtonContainer';
+
+// STYLING
+const btnStyle = `width:100%;height:28px;background:darkmagenta;font-family:inherit;font-size:small;border:none;margin-bottom:10px;border-radius:2px;color:white;cursor:pointer`;
+const btnTitle = 'Search on Libgen';
+
+// SEARCH URL
+const searchURL = bookTitle => `http://gen.lib.rus.ec/search.php?req=${bookTitle}&column=title&sort=year&sortmode=DESC`;
+
+// BOOK INFORMATION
+const bookTitle = document.querySelector(bookTitleSelector).childNodes[0].textContent.trim();
+const bookBtnContainer = document.querySelector(btnContainerSelector);
+
+// BUTTON
+const libgenBtn = new Element('button');
+libgenBtn.textContent = btnTitle;
+libgenBtn.setAttribute('style', btnStyle);
+libgenBtn.addEventListener('click', () => window.open(searchURL(bookTitle), '_blank'));
+
+// INIT
+bookBtnContainer.parentNode.insertBefore(libgenBtn, bookBtnContainer);
